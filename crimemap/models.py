52fe54ff6743d991b6d39fcdcfadf68780558a60
root <root@localhost.localdomain>
2016-06-21 21:21:40 +0530

    new file:   crimemap/__init__.py new file:   crimemap/admin.py 	new file:   crimemap/migrations/0001_initial.py 	new file:   crimemap/migrations/0002_pointofinterest.py 	new file:   crimemap/migrations/0003_crime_position.py 	new file:   crimemap/migrations/0004_auto_20160621_1348.py 	new file:   crimemap/migrations/0005_auto_20160621_1350.py 	new file:   crimemap/migrations/0006_remove_crime_type_of_crime.py 	new file:   crimemap/migrations/0007_crime_type_of_crime.py 	new file:   crimemap/migrations/0008_auto_20160621_1412.py 	new file:   crimemap/migrations/__init__.py 	new file:   crimemap/models.py 	new file:   crimemap/settings.py new file:   crimemap/templates/crimemap/add_route.html 	new file:   crimemap/templates/crimemap/index.html 	new file:   crimemap/urls.py 	new file:   crimemap/views.py 	new file:   crimemap/wsgi.py 	new file:   db.sqlite3 	new file:   db.sqlite3.old 	new file:   manage.py

---
+++ crimemap/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from geoposition.fields import GeopositionField
+
+class Crime(models.Model):
+    case_number = models.IntegerField('Case Number')
+    crime_date = models.DateTimeField('Date of Crime')
+    comp_name = models.CharField('Complainant Name',max_length=200)
+    #type_of_crime = models.CharField('Type of Crime',max_length=200)
+    type_of_crime_choice = (
+            ('as','Assault'),
+            ('br','Burglary'),
+            ('dp','Disturbing the Peace'),
+            ('dv','Drug Violation'),
+            ('hm','Homicide'),
+            ('rb','Robbery'),
+            ('th','Theft'),
+            ('vd','Vandalism'),
+            ('vt','Vehicle Theft'),
+            ('ms','Miscellaneous'),
+            )
+    type_of_crime = models.CharField('Type Of Crime',max_length=20,choices=type_of_crime_choice,default='t1')
+    position = GeopositionField('Location of Crime',default='28.4594965,77.0266383')
+    dec = models.TextField('Description',max_length=500)
+
